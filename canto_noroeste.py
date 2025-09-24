11c388987d9ffa4c64f95d4220f61b9b25eb2c2a
jruza <jruzacavalare@gmail.com>
2025-09-25 00:52:59 +0100

    init

--- canto_noroeste.py
+++ canto_noroeste.py
@@ -1,18 +1,18 @@
 def metodo_canto_noroeste(custos, ofertas, demandas):
     num_fontes = len(ofertas)
     num_destinos = len(demandas)
-
-    alocacoes = [[0] * num_destinos for _ in range(num_fontes)]
-
-    i = 0
-    j = 0
+    i, j = 0, 0
 
     ofertas_restantes = list(ofertas)
     demandas_restantes = list(demandas)
 
+    alocacoes = [
+        [0] * num_destinos
+        for _ in range(num_fontes)
+    ]
+
     while i < num_fontes and j < num_destinos:
         alocacao_atual = min(ofertas_restantes[i], demandas_restantes[j])
-
         alocacoes[i][j] = alocacao_atual
 
         ofertas_restantes[i] -= alocacao_atual
@@ -20,15 +20,13 @@ def metodo_canto_noroeste(custos, ofertas, demandas):
 
         if ofertas_restantes[i] == 0:
             i += 1
-
         if demandas_restantes[j] == 0:
             j += 1
 
-    custo_total = 0
-    for i in range(num_fontes):
-        for j in range(num_destinos):
-            custo_total += alocacoes[i][j] * custos[i][j]
+    custo_total = sum(
+        alocacoes[i][j] * custos[i][j]
+        for i in range(num_fontes)
+        for j in range(num_destinos)
+    )
 
     return alocacoes, custo_total
-
-
